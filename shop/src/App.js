a195a60d2d0f8a2baf60898023f6ba70ee103950
Munbear <pdwssf@gmail.com>
2021-08-26 18:37:55 +0900

    test

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,25 +1,131 @@
+/* eslint-disable */
 import logo from './logo.svg';
+import React, { useState, useContext } from 'react';
+import { Navbar, Container, Nav, NavDropdown, Button } from 'react-bootstrap';
 import './App.css';
+import Data from './data.js';
+import Detail from './Detail.js';
+import axios from 'axios';
+import { Link, Route, Switch } from 'react-router-dom';
+
+export let 재고context = React.createContext();
 
 function App() {
+  let [shoes, shoes변경] = useState(Data);
+  let [재고, 재고변경] = useState([10, 11, 12]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Navbar bg='light' expand='lg'>
+        <Container>
+          <Navbar.Brand href='#home'>Dowon shopping</Navbar.Brand>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='me-auto'>
+              <Nav.Link as={Link} to='/'>
+                Home
+              </Nav.Link>
+              <Nav.Link as={Link} to='./Detail'>
+                Detail
+              </Nav.Link>
+              <NavDropdown title='Dropdown' id='basic-nav-dropdown'>
+                <NavDropdown.Item href='#action/3.1'>Action</NavDropdown.Item>
+                <NavDropdown.Item href='#action/3.2'>
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href='#action/3.3'>
+                  Something
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href='#action/3.4'>
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      <Switch>
+        <Route exact path='/'>
+          <div className='background'>
+            <h1>Hello world</h1>
+            <p>
+              this is a simple hero unit, a simple jumbotron-style compoment for
+              calling i dont know what is wrong with this god damm code why is
+              it not working what i want to do i hope it will running that i
+              expected!
+            </p>
+            <p>
+              <Button>learn more</Button>
+            </p>
+          </div>
+
+          <div className='container'>
+            <재고context.Provider value={재고}>
+              <div className='row'>
+                {shoes.map((a, i) => {
+                  return <Card shoes={shoes[i]} i={i} key={i} />;
+                })}
+              </div>
+            </재고context.Provider>
+            <button
+              className='btn btn-primary'
+              onClick={() => {
+                axios
+                  .get('https://codingapple1.github.io./shop/data2.json')
+                  .then((result) => {
+                    console.log(result.data);
+                    shoes변경([...shoes, ...result.data]);
+                  })
+                  .catch(() => {
+                    console.log('실패');
+                  });
+              }}
+            >
+              더보기
+            </button>
+          </div>
+        </Route>
+
+        <Route path='/detail/:id'>
+          <재고context.Provider value={재고}>
+            <Detail shoes={shoes} 재고={재고} /> {/*  <<< 컴포넌트임 */}
+          </재고context.Provider>
+        </Route>
+
+        <Route path='/:id'>
+          <div>아무거나 적었을때 이거 보여주샘</div>
+        </Route>
+      </Switch>
     </div>
   );
 }
 
+function Card(props) {
+  let 재고 = useContext(재고context);
+  return (
+    <div className='col-m4'>
+      <img
+        src={
+          'https://codingapple1.github.io/shop/shoes' + (props.i + 1) + '.jpg'
+        }
+        width='100%'
+      />
+      <h4> {props.shoes.title}</h4>
+      <p>
+        {props.shoes.content} & {props.shoes.price}
+      </p>
+      <Test></Test>
+    </div>
+  );
+}
+
+function Test(props) {
+  let 재고 = useContext(재고context);
+  return <p>{재고[0]}</p>;
+}
+
 export default App;
+
+// https://codingapple1.github.io./shop/data2.json
---
+++ shop/src/Detail.js
@@ -0,0 +1,84 @@
+import axios from 'axios';
+import React, { useContext, useEffect, useState } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
+import styled from 'styled-components';
+import './Detail.scss';
+import { 재고context } from './App.js';
+
+let 박스 = styled.div`
+  padding: 20px;
+`;
+
+let 제목 = styled.h1`
+  font-size: 25px;
+  color: ${(props) => props.색상};
+`;
+
+function Detail(props) {
+  let [alert, alert변경] = useState(true);
+  let [inputData, inputData변경] = useState('');
+  let 재고 = useContext(재고context);
+
+  useEffect(() => {
+    let Timer = setTimeout(() => {
+      alert변경(false);
+    }, 2000);
+    return () => {
+      clearTimeout(Timer);
+    };
+  }, []);
+
+  let { id } = useParams();
+  let history = useHistory();
+  let findProduct = props.shoes.find(function (Product) {
+    return Product.id == id;
+  });
+
+  return (
+    <div className='container'>
+      <박스>
+        <제목 className='red'>Detail</제목>
+      </박스>
+      {inputData}
+      <input
+        onChange={(e) => {
+          inputData변경(e.target.value);
+        }}
+      />
+
+      {alert === true ? (
+        <div className='mt-alert'>
+          <p>재고가 얼마 남지 않았습니다.</p>
+        </div>
+      ) : null}
+
+      <div className='row'>
+        <div className='col-md-6'>
+          <img
+            src='https://codingapple1.github.io/shop/shoes1.jpg'
+            width='100%'
+          />
+        </div>
+        <div className='col-md-6 mt-4'>
+          <h4 className='pt-5'>{findProduct.title}</h4>
+          <p>{findProduct.content}</p>
+          <p>{findProduct.price}원</p>
+          <button className='btn btn-danger'>주문하기</button>
+          <button
+            className='btn btn-danger'
+            onClick={() => {
+              history.goBack();
+            }}
+          >
+            뒤로가기
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Info() {
+  return <p>재고 : ???</p>;
+}
+export default Detail;
