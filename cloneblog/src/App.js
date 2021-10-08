3f5686c2f6ea78911f2d505c33276741d7dba7a4
Munbear <pdwssf@gmail.com>
2021-10-08 20:20:08 +0900

    clone coding blog

--- cloneblog/src/App.js
+++ cloneblog/src/App.js
@@ -15,48 +15,104 @@ function App() {
     '강남 카페 추천',
   ]);
 
-  function 제목바꾸기() {
-    var newArray = [...글제목]; // 수정된[데이터]를 만들고 state를 deep copy해서 수정한다.
-    newArray[0] = '여자 코트 추천';
-    글제목변경(newArray);
-  }
-
   let [따봉, 따봉변경] = useState(0);
   let posts = '김포 고기 맛집';
+  let [modal, modal변경] = useState(false);
+  let [누른제목, 누른제목변경] = useState(0);
+  let [입력값, 입력값변경] = useState('');
 
   return (
     <div className='App'>
       <div className='black-nav'>
         <div>개발 Blog</div>
       </div>
-      <button onClick={제목바꾸기}>버튼</button>
-      <div className='list'>
-        <h3>
-          {글제목[0]}
-          <span
-            onClick={() => {
-              따봉변경(따봉 + 1);
-            }}
-          >
-            👍
-          </span>
-          {따봉}
-        </h3>
-        <p>10월 4일 발행</p>
-        <hr />
+      {글제목.map(function (글, i) {
+        return (
+          <div className='list' key={i}>
+            <h3
+              onClick={() => {
+                누른제목변경(i);
+              }}
+            >
+              {글}
+              <span
+                onClick={() => {
+                  따봉변경(따봉 + 1);
+                }}
+              >
+                👍
+              </span>
+              {따봉}
+            </h3>
+            <p>10월 3일 발행</p>
+            <hr />
+          </div>
+        );
+      })}
+
+      <div className='publish'>
+        <input
+          onChange={(e) => {
+            입력값변경(e.target.value);
+          }}
+        />
+        <button
+          onClick={() => {
+            var arrayCopy = [...글제목];
+            arrayCopy.unshift(입력값); // unshift는 앞에 값에 추가 해주세요 라는 함수
+            글제목변경(arrayCopy);
+          }}
+        >
+          저장
+        </button>
       </div>
-      <div className='list'>
-        <h3> {글제목[1]} </h3>
-        <p>10월 3일 발행</p>
-        <hr />
-      </div>
-      <div className='list'>
-        <h3> {글제목[2]} </h3>
-        <p>10월 2일 발행</p>
-        <hr />
-      </div>
+
+      <Profile />
+
+      <button
+        onClick={() => {
+          modal변경(!modal);
+        }}
+      >
+        열고닫기
+      </button>
+      {modal === true ? (
+        <Modal 글제목={글제목} 누른제목={누른제목}></Modal>
+      ) : null}
     </div>
   );
+}
+
+function Modal(props) {
+  return (
+    <div className='modal'>
+      <h2>{props.글제목[props.누른제목]}</h2>
+      <p>날짜</p>
+      <p>상세내용 </p>
+    </div>
+  );
+}
+
+class Profile extends React.Component {
+  //// 옛날 문법
+  constructor() {
+    super();
+    this.state = { name: 'Park', age: 30 };
+  }
+
+  changeName = () => {
+    this.setState({ name: 'MUN' });
+  };
+
+  render() {
+    return (
+      <div>
+        <h3>프로필입니다.</h3>
+        <p>저는 {this.state.name} 입니다.</p>
+        <button onClick={this.changeName}> 버튼 </button>
+      </div>
+    );
+  }
 }
 
 export default App;
