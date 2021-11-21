7ec852fbc1368f3da55c64295f97afa42e062dd8
Munbear <pdwssf@gmail.com>
2021-11-21 18:35:54 +0900

    tab styling

--- clone-lp/src/App.js
+++ clone-lp/src/App.js
@@ -166,25 +166,35 @@ function TabContent(props) {
   if (props.tab === 0) {
     return (
       <div className='products__list'>
-        <img src={product1} width='100%' />
-        <p className='products__title'>{props.product.title}</p>
-        <h4 className='products__price'>{props.product.price}</h4>
+        <div className='products__card'>
+          <img src={product1} width='100%' />
+          <p className='products__title'>{props.product.title}</p>
+          <h4 className='products__price'>{props.product.price}</h4>
+        </div>
       </div>
     );
   } else if (props.tab === 1) {
     return (
       <div className='mall__list'>
-        <p className='mall__rank'>{props.mall.rank}</p>
-        <h4 className='mall__title'>{props.mall.title}</h4>
-        <img className='mall__img' src={mall1} width='26%' />
+        <div className='mall__info'>
+          <p className='mall__rank'>{props.mall.rank}</p>
+          <h2 className='mall__title'>{props.mall.title}</h2>
+        </div>
+        <div className='mall__card'>
+          <img className='mall__img' src={mall1} width='100%' />
+        </div>
       </div>
     );
   } else if (props.tab === 2) {
     return (
       <div className='brand__list'>
-        <p className='brand__rank'>{props.brand.rank}</p>
-        <h4 className='bradn__title'>{props.brand.title}</h4>
-        <img className='brand__img' src={brand1} />
+        <div className='brand__info'>
+          <p className='brand__rank'>{props.brand.rank}</p>
+          <h2 className='brand__title'>{props.brand.title}</h2>
+        </div>
+        <div className='brand__card'>
+          <img className='brand__img' src={brand1} />
+        </div>
       </div>
     );
   }
