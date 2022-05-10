b31d28e494369055e92bb43e4c5fb02a7f27c0b1
Munbear <pdwssf@gmail.com>
2022-05-10 20:53:27 +0900

    set sass

---
+++ wol/src/components/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/HeaderStyle.sass'
+
+const Header = () => {
+    return (
+        <>
+            <div className='container'>
+                <header>Work out is my life!</header>
+            </div>
+
+        </>
+    )
+}
+
+export default Header;
