bc775f3a48165f6f91a3fbb29cb6961044c9ed3b
Munbear <pdwssf@gmail.com>
2022-07-11 20:55:07 +0900

    setting sass

---
+++ planner/src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return(
+        <header className='test'>
+            <h1>hello world</h1>
+        </header>
+    )
+}
+
+export default Header
