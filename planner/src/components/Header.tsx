e0df637d35855e15a6c72f4df7c7da1200e2ca72
Munbear <pdwssf@gmail.com>
2022-07-12 18:01:16 +0900

    reset create app

---
+++ planner/src/components/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../styels/HeaderLayout.scss'
+
+const Header = () => {
+    return(
+        <>
+            <header className='header-container'>
+                <h1>planner</h1>
+            </header>
+            <h2>asdfad</h2>
+        </>
+    )
+}
+
+export default Header
