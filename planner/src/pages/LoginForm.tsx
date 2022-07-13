bcf1c62f1bb430b79b4cd5e6f07a7f5a5e761269
Munbear <pdwssf@gmail.com>
2022-07-13 19:45:07 +0900

    routing

--- planner/src/pages/LoginForm.tsx
+++ planner/src/pages/LoginForm.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import { useNavigate} from "react-router-dom";
 import '../styels/LoginStyle/LoginFormStyle.scss'
 
 const LoginForm = () => {
+
+    const navigate = useNavigate()
+    const submit = () => {
+        navigate('/home')
+    }
+
     return (
         <>
             <section className='container'>
@@ -10,7 +17,7 @@ const LoginForm = () => {
                     <input className='input-text' type='text' placeholder='Email'/>
                     <input className='input-pw' type='password' placeholder='Password'/>
                     <form className='submit-form'>
-                        <button className='submit-btn'>Submit</button>
+                        <button className='submit-btn' onClick={submit}>Submit</button>
                     </form>
                 </div>
             </section>
